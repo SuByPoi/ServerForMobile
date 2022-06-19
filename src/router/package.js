cfe23066384d11eca4c68d337a3548e550033f1e
thailoc <thailoc321@gmail.com>
2022-06-19 12:37:27 +0700

    first commit

---
+++ src/router/package.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {GetQuestionInPackageFocusRule,GetAllPackages,GetPackage,GetQuestionInPackage,CreateQuestionForPackage,CreatePackage,EditPackage,DeletePackage } = require('../controller/package');
+
+router.get('/',GetAllPackages);
+router.get('/:id',GetPackage);
+router.get('/:id/question/:rule',GetQuestionInPackageFocusRule);
+router.get('/:id/question',GetQuestionInPackage);
+router.post('/:id/question/create',CreateQuestionForPackage);
+router.post('/',CreatePackage);
+router.patch('/:id',EditPackage);
+router.delete('/:id',DeletePackage);
+
+module.exports = router;
